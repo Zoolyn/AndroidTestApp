5083d2a9d792983d1ae0dc619194105aef38cc16
Jerry <zoolyn.cs@gmail.com>
2021-02-20 11:46:14 -0500

    Adjusted Main Activity Styles

--- AndroidTest/app/src/main/java/com/datechnologies/androidtest/chat/ChatActivity.java
+++ AndroidTest/app/src/main/java/com/datechnologies/androidtest/chat/ChatActivity.java
@@ -13,6 +13,14 @@ import com.datechnologies.androidtest.MainActivity;
 import com.datechnologies.androidtest.R;
 import com.datechnologies.androidtest.api.ChatLogMessageModel;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
